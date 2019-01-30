37fe2a084577afc73cb95acd88abb32290ba3105
github username <github email address>
2019-01-30 11:34:19 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import {getAll, getByType} from '../data/pets.js'
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
+import { get } from 'https';
+
+let updateElementInArray = (array, id, values) => {
+  return array.map( element => {
+    if(element.id == id){
+      return { ...element, ...values }
+    } else {
+      return element
+    }
+  })
+}
 
 class App extends React.Component {
   constructor() {
@@ -15,7 +26,30 @@ class App extends React.Component {
     }
   }
 
+  adoptPet = (id) => {
+    this.setState({
+      pets: updateElementInArray(this.state.pets, id, {isAdopted: true})
+    })
+  }
+  onChangeType = (e) => {
+    let type = e.target.value
+    this.setState({filters: {type: type}})
+  }
+  
+  fetchData = () => {
+    if(this.state.filters.type !== 'all'){
+      this.setState({pets: getByType(this.state.filters.type)})
+    }
+    else{
+      this.setState({pets: getAll()})
+    }
+  }
+
+  
+
+  
   render() {
+    console.log(this.state.pets)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +58,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} filters={this.state.filters} fetchData={this.fetchData}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser fetchData={this.fetchData} pets={this.state.pets} adoptPet={this.adoptPet}/>
             </div>
           </div>
         </div>
